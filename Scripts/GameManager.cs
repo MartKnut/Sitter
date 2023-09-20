797fc7565e3ff0e935cc8d40b62259b9aaac913d
Martink <martin.knutsen@live.no>
2023-09-20 16:31:37 +0200

    Timer and Game Manager
    In Game Manager Object
    + Timer obj
    - "Move()"
    moved code from "Move()" to _on_time_passed

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -1,8 +1,11 @@
 using Godot;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using Sitter.Scripts;
+using Timer = Godot.Timer;
 
 public partial class GameManager : Node
 {
@@ -11,81 +14,36 @@ public partial class GameManager : Node
 	[Export()] public float PlayerSeatDistance = 30;
 	[Export()] public AnimatedSprite2D Player;
 	[Export()] public Seat[] Seats;
-	[Export()] public float TimeToMove;
-
+	[Export()] public float TimeToMove = 1;
+	[Export()] public float timeMultiplier = 0.90f;
+	
 	private bool _moveRight = false;
 	private float currentTime;
 	private int currentSeatPos = 1;
-	
+	private Timer Timer;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Player = GetParent().GetChildren().OfType<PlayerInput>().First();
+		Timer = GetChild<Timer>(0);
+
+		Player = GetParent().GetChildren().OfType<Player>().First();
 		Player.FlipH = _moveRight;
 		
 		List<Seat> seats = GetParent().GetChildren().OfType<Seat>().ToList();
 		Seats = seats.ToArray();
 		Player.Position = new Vector2(Seats[seats.Count / 2].Position.X, Seats[seats.Count / 2].Position.Y + PlayerSeatDistance);
+		
+		Timer.WaitTime = TimeToMove;
+		Timer.Start();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		
-		Move(delta);
 		Sit();
 	}
-
-	private void Move(double delta)
-	{
-		float direction = Input.GetAxis("Left", "Right");
-		
-		if (Input.IsActionJustPressed("Left") || Input.IsActionJustPressed("Right"))
-		{
-			currentSeatPos += 1 * (int)direction;
-			
-			if (currentSeatPos >= Seats.Length) 
-				currentSeatPos = Seats.Length - 1;
-			
-			if (currentSeatPos <= 0) 
-				currentSeatPos = 0;
-
-			if (Player != null)
-			{
-				Player.Position = new Vector2(Seats[currentSeatPos].Position.X, Seats[currentSeatPos].Position.Y + PlayerSeatDistance);
-			}
-			
-			
-		}
-
-		currentTime += (float)delta;
-		
-		if (currentTime >= TimeToMove)
-		{
-			if (currentSeatPos <= 0 || currentSeatPos >= Seats.Length - 1)
-			{
-				_moveRight = !_moveRight;
-				Player.FlipH = _moveRight;
-			}
-				
-
-			if (_moveRight)
-			{
-				currentSeatPos++;
-				Player.Position = new Vector2(Seats[currentSeatPos].Position.X, Seats[currentSeatPos].Position.Y + PlayerSeatDistance);
-			}
-			else
-			{
-				currentSeatPos--;
-				Player.Position = new Vector2(Seats[currentSeatPos].Position.X, Seats[currentSeatPos].Position.Y + PlayerSeatDistance);
-			}
-			
-			currentTime = 0;
-		}
-
-
-	}
+	
 	private void Sit()
 	{
 		if (Input.IsActionJustPressed("Sit"))
@@ -105,8 +63,9 @@ public partial class GameManager : Node
 				AddChild(Player);
 
 				Player.Position = new Vector2(Seats[currentSeatPos].Position.X, Seats[currentSeatPos].Position.Y + PlayerSeatDistance);
-				
-				TimeToMove *= 0.90f;
+
+				TimeToMove *= timeMultiplier;
+				Timer.WaitTime = TimeToMove;
 			}
 			else
 			{
@@ -118,5 +77,28 @@ public partial class GameManager : Node
 	private void Lose(int Score)
 	{
 		GetTree().Quit();
+	}
+
+	void _on_time_passed()
+	{
+		
+		if (currentSeatPos <= 0 || currentSeatPos >= Seats.Length - 1) 
+		{
+			_moveRight = !_moveRight;
+			Player.FlipH = _moveRight;
+		}
+		
+		if (_moveRight) 
+		{
+			currentSeatPos++;
+			Player.Position = new Vector2(Seats[currentSeatPos].Position.X, Seats[currentSeatPos].Position.Y + PlayerSeatDistance);
+		}
+		else
+		{
+			currentSeatPos--;
+			Player.Position = new Vector2(Seats[currentSeatPos].Position.X, Seats[currentSeatPos].Position.Y + PlayerSeatDistance);
+		}
+			
+		Timer.Start();
 	}
 }
--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,21 +1,15 @@
 using Godot;
+using System;
 
-namespace Sitter.Scripts;
-
-public partial class PlayerInput : AnimatedSprite2D
+public partial class Player : AnimatedSprite2D
 {
-
-	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		
-
 	}
 }
